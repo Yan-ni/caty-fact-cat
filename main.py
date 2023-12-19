8379a4c50fa10dd73c1b881231057534c51a10e3
Anis <contact@aniskh.com>
2023-12-19 22:22:28 +0100

    refactor

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-import requests
-import json
+
+# Imports
 from dotenv import load_dotenv
 import os
+from urllib import parse
+import requests
+import json
 from io import BytesIO
 from PIL import Image
-from urllib import parse
 
+# Load environement variables
 load_dotenv()
 
-hashtags = ['cat', 'cats', 'catsofinstagram', 'cats_of_instagram', 'catlove', 'catlover', 'catlovers', 'catloversclub', 'catlife', 'cats_of_world', 'catphoto', 'instacat', 'catoftheday', 'cats_of_day', 'catstagram', 'catsagram']
+# Global variables
+IG_USER_ID = os.getenv('IG_USER_ID')
+IG_ACCESS_TOKEN = os.getenv('IG_ACCESS_TOKEN')
+IG_API_VERSION = os.getenv('IG_API_VERSION')
+CAT_IMAGE_API_KEY = os.getenv('CAT_IMAGE_API_KEY')
 
+# Functions
 def get_fact() -> str:
-	"""Requests cat fact api
+	"""Requests a cat fact from the api
 
 	Returns:
-		str: the fact with "Did you know ! " at the beginning
+		str: the fact concatenated with "Did you know ! " at the start of the string
 	"""
 	api_url_cat_fact = f'https://meowfacts.herokuapp.com/'
 	
@@ -25,23 +33,23 @@ def get_fact() -> str:
 
 	return f'Did you know ! {cat_fact}'
 
-def get_caption() -> str:
+def get_ig_caption() -> str:
+	hashtags = ['cat', 'cats', 'catsofinstagram', 'cats_of_instagram', 'catlove', 'catlover', 'catlovers', 'catloversclub', 'catlife', 'cats_of_world', 'catphoto', 'instacat', 'catoftheday', 'cats_of_day', 'catstagram', 'catsagram']
 	fact = get_fact()
 
 	return f'{fact}\r\n\r\n{" ".join([f"#{hashtag}" for hashtag in hashtags])}'
 
-def get_image() -> str:
+def get_cat_image_url() -> str:
 	"""Requests cat iamge api until getting a valid image to post
 
 	Returns:
 		str: the img url
 	"""
-	api_key = os.getenv('API_KEY')
 	image_extension = ''
 	image_width = 0
 
 	while image_extension not in ['.jpg', 'jpeg'] or image_width <= 320 or image_width >= 1440:
-		response = requests.get(f'https://api.thecatapi.com/v1/images/search?api_key={api_key}')
+		response = requests.get(f'https://api.thecatapi.com/v1/images/search?api_key={CAT_IMAGE_API_KEY}')
 		cat_image_url = response.json()[0]["url"]
 		image_extension = cat_image_url[-4:]
 
@@ -49,43 +57,45 @@ def get_image() -> str:
 		img = Image.open(BytesIO(response.content))
 
 		image_width, _ = img.size
-	
+
 	return cat_image_url
 
-if __name__ == '__main__':
-	ig_user_id = os.getenv('IG_USER_ID')
-
-	try:
-		print('getting cat image...')
-		image_url = get_image()
-		print('getting cat fact...')
-		caption = get_caption()
-	except e:
-		print('failed.')
-		print(e)
-		exit()
-
-	access_token = os.getenv('ACCESS_TOKEN')
-
-	print('creating instagram media...')
+def create_ig_media(image_url: str, caption= ""):
 	image_url = parse.quote(image_url, safe='')
 	caption = parse.quote(caption, safe='')
-	res = requests.post(f'https://graph.facebook.com/v15.0/{ig_user_id}/media?image_url={image_url}&caption={caption}&access_token={access_token}')
-	res = res.json()
+	
+	res = requests.post(f'https://graph.facebook.com/{IG_API_VERSION}/{IG_USER_ID}/media?image_url={image_url}&caption={caption}&access_token={IG_ACCESS_TOKEN}')
+	
+	return res.json()
 
-	if 'id' not in res:
+def publish_ig_media(media_id: str):
+	res = requests.post(f'https://graph.facebook.com/{IG_API_VERSION}/{IG_USER_ID}/media_publish?creation_id={media_id}&access_token={IG_ACCESS_TOKEN}')
+
+	return res.json()
+
+# Main function
+if __name__ == '__main__':
+	try:
+		print('getting cat image...')
+		cat_image_url = get_cat_image_url()
+
+		print('getting cat fact...')
+		caption = get_ig_caption()
+
+		print('creating instagram media...')
+		ig_media_response = create_ig_media(cat_image_url, caption)
+
+		if 'id' not in ig_media_response:
+			raise Exception(json.dumps(ig_media_response, indent=2))
+
+		print('publishing the media...')
+		ig_media_id = ig_media_response['id']
+		ig_publish_response = publish_ig_media(ig_media_id)
+		
+		if 'id' not in ig_publish_response:
+			raise Exception(json.dumps(ig_publish_response, indent=2))
+
+		print('media published successfully.')
+	except Exception as e:
 		print('failed.')
-		print(json.dumps(res, indent=2))
-		exit()
-
-	media_id = res['id']
-	print('publishing the media...')
-	res = requests.post(f'https://graph.facebook.com/v15.0/{ig_user_id}/media_publish?creation_id={media_id}&access_token={access_token}')
-	res = res.json()
-
-	if 'id' not in res:
-		print('failed.')
-		print(json.dumps(res, indent=2))
-		exit()
-
-	print('media published successfully.')
+		print(e)
