e271a5a454bc0c5fd425cb0220ef2792956851ab
Anis <contact@aniskh.com>
2023-12-21 10:51:45 +0100

    refactor & document functions

--- main.py
+++ main.py
@@ -18,11 +18,7 @@ CAT_IMAGE_API_KEY = os.getenv('CAT_IMAGE_API_KEY')
 
 # Functions
 def get_fact() -> str:
-	"""Requests a cat fact from the api
-
-	Returns:
-		str: the fact concatenated with "Did you know ! " at the start of the string
-	"""
+	"""Requests a cat fact from cat fact API and returns the fact concatenated with "Did you know ! " at the start of the fact."""
 	api_url_cat_fact = f'https://meowfacts.herokuapp.com/'
 	
 	response = requests.get(api_url_cat_fact)
@@ -32,17 +28,14 @@ def get_fact() -> str:
 	return f'Did you know ! {cat_fact}'
 
 def get_ig_caption() -> str:
+	"""Genrate the instagram post caption with the cat fact and hashtags"""
 	hashtags = ['cat', 'cats', 'catsofinstagram', 'cats_of_instagram', 'catlove', 'catlover', 'catlovers', 'catloversclub', 'catlife', 'cats_of_world', 'catphoto', 'instacat', 'catoftheday', 'cats_of_day', 'catstagram', 'catsagram']
 	fact = get_fact()
 
 	return f'{fact}\r\n\r\n{" ".join([f"#{hashtag}" for hashtag in hashtags])}'
 
 def get_cat_image_url() -> str:
-	"""Requests cat iamge api until getting a valid image to post
-
-	Returns:
-		str: the img url
-	"""
+	"""Requests a cat iamge from the cat image API and returns the cat image url"""
 	cat_image_extension = ''
 	cat_image_width = 0
 
@@ -56,18 +49,28 @@ def get_cat_image_url() -> str:
 
 	return cat_image_url
 
-def create_ig_media(image_url: str, caption= ""):
+def create_ig_media(image_url: str, caption= "") -> str:
+	"""Creates an Instagram media using Instagram graph API"""
 	image_url = parse.quote(image_url, safe='')
 	caption = parse.quote(caption, safe='')
 	
 	res = requests.post(f'https://graph.facebook.com/{IG_API_VERSION}/{IG_USER_ID}/media?image_url={image_url}&caption={caption}&access_token={IG_ACCESS_TOKEN}')
-	
-	return res.json()
+	res = res.json()
 
-def publish_ig_media(media_id: str):
+	ig_media_id = res.get('id')
+
+	if not ig_media_id:
+		raise Exception(json.dumps(res, indent=2))
+
+	return ig_media_id
+
+def publish_ig_media(media_id: str) -> None:
+	"""Publishes an Instagram media using Instagram graph API"""
 	res = requests.post(f'https://graph.facebook.com/{IG_API_VERSION}/{IG_USER_ID}/media_publish?creation_id={media_id}&access_token={IG_ACCESS_TOKEN}')
+	res = res.json()
 
-	return res.json()
+	if 'id' not in res:
+		raise Exception(json.dumps(res, indent=2))
 
 # Main function
 if __name__ == '__main__':
@@ -79,17 +82,10 @@ if __name__ == '__main__':
 		caption = get_ig_caption()
 
 		print('creating instagram media...')
-		ig_media_response = create_ig_media(cat_image_url, caption)
-
-		if 'id' not in ig_media_response:
-			raise Exception(json.dumps(ig_media_response, indent=2))
+		ig_media_id = create_ig_media(cat_image_url, caption)
 
 		print('publishing the media...')
-		ig_media_id = ig_media_response['id']
-		ig_publish_response = publish_ig_media(ig_media_id)
-		
-		if 'id' not in ig_publish_response:
-			raise Exception(json.dumps(ig_publish_response, indent=2))
+		publish_ig_media(ig_media_id)
 
 		print('media published successfully.')
 	except Exception as e:
